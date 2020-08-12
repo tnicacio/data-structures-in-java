d9640ca84886869d76963ddf5a320c94d60f4d7c
Tiago Nicacio <nicacio.t92@gmail.com>
2020-08-11 22:04:01 -0300

    insert and toString methods

--- src/arvorebinariabusca/ArvoreBinariaBusca.java
+++ src/arvorebinariabusca/ArvoreBinariaBusca.java
@@ -17,6 +17,20 @@ public class ArvoreBinariaBusca {
 	}
 
 	public void insere(int v) {
+		this.raiz = insereAux(this.raiz, v);
+	}
+	
+	private NoArvoreBinaria insereAux(NoArvoreBinaria no, int valor) {
+		if (no == null) {
+			no = new NoArvoreBinaria(valor);
+		} else {
+			if (valor < no.getInfo()) {
+				no.setSae(insereAux(no.getSae(), valor));
+			} else {
+				no.setSad(insereAux(no.getSad(),valor));
+			}
+		}
+		return no;
 	}
 
 	private NoArvoreBinaria insere(NoArvoreBinaria a, int v) {
@@ -31,7 +45,18 @@ public class ArvoreBinariaBusca {
 	}
 
 	public String toString() {
-		return "";
+		StringBuilder sb = new StringBuilder();
+		return imprime(this.raiz, sb).toString();
+	}
+	
+	
+	private StringBuilder imprime(NoArvoreBinaria no, StringBuilder sb) {
+		if (no != null) {
+			imprime(no.getSae(),sb);
+			sb.append(no.getInfo()).append(" ");
+			imprime(no.getSad(),sb);
+		}
+		return sb;
 	}
 
 }
---
+++ src/arvorebinariabusca/TesteArvoreBinariaBusca.java
@@ -0,0 +1,16 @@
+package arvorebinariabusca;
+
+public class TesteArvoreBinariaBusca {
+
+	public static void main(String[] args) {
+		
+		ArvoreBinariaBusca ab = new ArvoreBinariaBusca();
+		ab.insere(1);
+		ab.insere(2);
+		ab.insere(4);
+		ab.insere(3);
+		System.out.println(ab);
+
+	}
+
+}
