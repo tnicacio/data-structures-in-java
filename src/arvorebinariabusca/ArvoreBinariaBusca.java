1eec1d92417c414611165ce29465edba5a790a43
Tiago Nicacio <nicacio.t92@gmail.com>
2020-08-11 21:47:35 -0300

    search binary tree - initial structure

---
+++ src/arvorebinariabusca/ArvoreBinariaBusca.java
@@ -0,0 +1,37 @@
+package arvorebinariabusca;
+
+public class ArvoreBinariaBusca {
+
+	private NoArvoreBinaria raiz;
+
+	public ArvoreBinariaBusca() {
+		this.raiz = null;
+	}
+
+	public NoArvoreBinaria busca(int v) {
+		return raiz;
+	}
+
+	private NoArvoreBinaria busca(NoArvoreBinaria a, int v) {
+		return a;
+	}
+
+	public void insere(int v) {
+	}
+
+	private NoArvoreBinaria insere(NoArvoreBinaria a, int v) {
+		return a;
+	}
+
+	public void retira(int v) {
+	}
+
+	private NoArvoreBinaria retira(NoArvoreBinaria a, int v) {
+		return a;
+	}
+
+	public String toString() {
+		return "";
+	}
+
+}
---
+++ src/arvorebinariabusca/NoArvoreBinaria.java
@@ -0,0 +1,46 @@
+package arvorebinariabusca;
+
+public class NoArvoreBinaria {
+	
+	private int info;
+	private NoArvoreBinaria sae;
+	private NoArvoreBinaria sad;
+	
+	public NoArvoreBinaria(int info) {
+		this.info = info;
+		this.sae = null;
+		this.sad = null;
+	}
+	
+	public NoArvoreBinaria(int info, NoArvoreBinaria esq, NoArvoreBinaria dir) {
+		this.info = info;
+		this.sae = esq;
+		this.sad = dir;
+	}
+	
+	public void setInfo(int info) {
+		this.info = info;
+	}
+	public int getInfo() {
+		return this.info;
+	}
+	
+	public void setSae(NoArvoreBinaria esq) {
+		this.sae = esq;
+	}
+	public NoArvoreBinaria getSae() {
+		return this.sae;
+	}
+	
+	public void setSad(NoArvoreBinaria dir) {
+		this.sad = dir;
+	}
+	public NoArvoreBinaria getSad() {
+		return this.sad;
+	}
+	
+	public String toString() {
+		return new StringBuilder().append(this.info).toString();
+	}
+	
+}
